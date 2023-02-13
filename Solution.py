152aefe701e1e09c05bd99b78616b9d2e4dab695
Sadaf Saleem <sadafsaleem5815@gmail.com>
2023-02-13 23:32:20 +0500

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -64,6 +64,36 @@ the linear regression equation that best fit the training data.
 After the model has been trained, we can use it to make predictions on new data by calling the predict method.
 '''
 
+tree = DecisionTreeRegressor(max_depth=12,min_samples_split=2,random_state=42)
+tree.fit(X_train,y_train)
+y_pred2 = tree.predict(X_test)
+
+'''
+This code block is defining and training a Decision Tree Regression model. The model is defined using the DecisionTreeRegressor class from the scikit-learn 
+library and the parameters for the model are passed as arguments to the class constructor. The max_depth parameter specifies the maximum depth of the tree 
+and the min_samples_split parameter is used to control the minimum number of samples required to split an internal node. The random_state is used to set the 
+random number generator seed, which helps in reproducing the same results if the same seed is used.
+The fit function is used to train the decision tree model using the training data X_train and y_train. Once the model is trained, 
+it can be used to make predictions on the test data X_test using the predict function. The predictions are stored in the variable y_pred2.
+The goal of using Decision Tree Regression is to create a model that predicts a target value based on several input features. 
+Decision Trees work by recursively splitting the data into smaller and smaller subsets based on the value of one of the input features. 
+The splits are performed in such a way that they minimize the variance in the target variable. The final result is a tree of splits and leaves, 
+where each leaf node represents a prediction for the target variable based on the input features.
+'''
+
+d_r2 = tree.score(X_test, y_test)
+print("Decision Tree Regressor R-squared: {}".format(d_r2))
+
+d_mse = mean_squared_error(y_pred2, y_test)
+d_rmse = np.sqrt(d_mse)
+print("Decision Tree Regressor RMSE: {}".format(d_rmse))
+
+'''
+The code is evaluating the performance of the Decision Tree Regressor model.
+The first line computes the R-squared value, which is a measure of the goodness of fit of the model, with a value of 1 indicating a perfect fit and a value of 0 indicating a poor fit. The R-squared value is printed using the "format" function to insert the value into the string.
+The next two lines compute the mean squared error (MSE) and the root mean squared error (RMSE) between the predicted values (y_pred2) and the actual values (y_test). The MSE is a measure of the average difference between the predicted and actual values, and the RMSE is the square root of the MSE. Both values are printed using the "format" function to insert the values into the strings.
+'''
+
 ### Finding the feature importance
 resultdict = {}
 for i in range(len(feature_cols)):
@@ -81,3 +111,43 @@ as the bar heights. The plt.xticks function is used to rotate the x-axis labels 
 to add a title to the plot.
 
 '''
+train_score = []
+test_score = []
+max_score = 0
+max_pair = (0,0)
+
+for i in range(1,50):
+    tree = DecisionTreeRegressor(max_depth=i,random_state=42)
+    tree.fit(X_train,y_train)
+    y_pred = tree.predict(X_test)
+    train_score.append(tree.score(X_train,y_train))
+    test_score.append(r2_score(y_test,y_pred))
+    test_pair = (i,r2_score(y_test,y_pred))
+    if test_pair[1] > max_pair[1]:
+        max_pair = test_pair
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(1,50), train_score, label = "Training R^2",color='lightcoral')
+ax.plot(np.arange(1,50), test_score, label = "Testing R^2",color='lime')
+print(f'Best max_depth is: {max_pair[0]} \nTesting R^2 is: {max_pair[1]}')
+
+'''
+This code defines two lists, train_score and test_score, to store the R-squared values of the Decision Tree Regressor for the training and testing sets, 
+respectively. It then loops over the range of 1 to 50 for the max_depth parameter in the Decision Tree Regressor and computes the R-squared value for both 
+the training and testing sets. The R-squared value for the training set is computed using the tree.score method, while the R-squared value for the testing set 
+is computed using the r2_score method from the sklearn.metrics module. The R-squared values are stored in the train_score and test_score lists.
+Additionally, the code also keeps track of the maximum R-squared value for the testing set and the corresponding max_depth value, which are stored in the 
+max_pair tuple. The if statement updates the max_pair tuple if a new maximum R-squared value is found.
+Finally, the code plots the R-squared values for both the training and testing sets, and prints out the best max_depth value and the corresponding testing R-squared value.
+'''
+
+importance = tree.feature_importances_
+
+f_importance = {}
+for i in range(len(feature_cols)):
+     f_importance[feature_cols[i]] = importance[i]
+        
+plt.bar(f_importance.keys(),f_importance.values())
+plt.xticks(rotation='vertical')
+plt.title('Feature Importance in Decision Tree Regression Model');
+
